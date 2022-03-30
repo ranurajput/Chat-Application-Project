d25b13d72286cc38228ec7d4285b5d09a244a9c5
RANU RAJPUT <83179328+ranurajput@users.noreply.github.com>
2022-03-30 14:50:55 +0530

    Update WebSocketConfiguration.java

--- ChatApplication/src/main/java/com/example/configuration/WebSocketConfiguration.java
+++ ChatApplication/src/main/java/com/example/configuration/WebSocketConfiguration.java
@@ -1,15 +1,25 @@
 package com.example.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfiguration implements WebSocketConfigurer {
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+      //  WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
     }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+       // WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+        registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
+    }
+    
 }
